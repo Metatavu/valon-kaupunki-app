912238c4e985a9d0dd01c96d9783c1bc4b8b659c
apachejuice <valio.valtokari@gmail.com>
2023-07-27 10:12:29 +0300

    Enable scrolling of attraction and coupon/benefit content

--- lib/widgets/attraction_info_overlay.dart
+++ lib/widgets/attraction_info_overlay.dart
@@ -33,121 +33,126 @@ class AttractionInfoOverlay extends StatelessWidget {
         child: SizedBox.expand(
           child: Container(
             color: Colors.black38,
-            child: Column(
-              children: [
-                AppBar(
-                  backgroundColor: Colors.black,
-                  leading: IconButton(
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                backgroundColor: Colors.black,
+                leading: IconButton(
+                  icon: const Icon(
+                    Icons.close,
+                    color: Colors.white,
+                  ),
+                  onPressed: onClose,
+                ),
+                title: Center(
+                  child: Text(
+                    attraction.title,
+                    style: theme.textTheme.bodyMedium,
+                  ),
+                ),
+                actions: [
+                  IconButton(
                     icon: const Icon(
-                      Icons.close,
+                      Icons.open_in_full,
                       color: Colors.white,
                     ),
-                    onPressed: onClose,
+                    onPressed: () {},
                   ),
-                  title: Center(
-                    child: Text(
-                      attraction.title,
-                      style: theme.textTheme.bodyMedium,
-                    ),
-                  ),
-                  actions: [
-                    IconButton(
-                      icon: const Icon(
-                        Icons.open_in_full,
-                        color: Colors.white,
+                ],
+              ),
+              body: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    if (attraction.image != null)
+                      HeightConstrainedImage.network(
+                        height: 200,
+                        radius: 00,
+                        url: attraction.image!.image.url,
                       ),
-                      onPressed: () {},
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              attraction.subTitle,
+                              textAlign: TextAlign.left,
+                              style: theme.textTheme.bodyMedium,
+                            ),
+                            if (attraction.description != null)
+                              Padding(
+                                padding: const EdgeInsets.only(top: 8.0),
+                                child: Text(
+                                  attraction.description!,
+                                  textAlign: TextAlign.left,
+                                  style: theme.textTheme.bodySmall,
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
                     ),
+                    PropertyInfo(
+                      leading: SvgPicture.asset(
+                        Assets.attractionsIconAsset,
+                        width: 24.0,
+                        height: 24.0,
+                        colorFilter: const ColorFilter.mode(
+                            Colors.white, BlendMode.srcIn),
+                      ),
+                      title: localizations.category,
+                      text: getAttractionCategoryLabel(
+                          attraction.category, localizations),
+                      trailing: null,
+                    ),
+                    if (attraction.address != null)
+                      PropertyInfo(
+                        leading: SvgPicture.asset(
+                          Assets.homeIconAsset,
+                          width: 24.0,
+                          height: 24.0,
+                          colorFilter: const ColorFilter.mode(
+                              Colors.white, BlendMode.srcIn),
+                        ),
+                        title: localizations.address,
+                        text: attraction.address!,
+                        trailing: null,
+                      ),
+                    PropertyInfo(
+                      leading: SvgPicture.asset(
+                        Assets.locationIconAsset,
+                        width: 24.0,
+                        height: 24.0,
+                        colorFilter: const ColorFilter.mode(
+                            Colors.white, BlendMode.srcIn),
+                      ),
+                      title: localizations.distanceToTarget,
+                      text: currentLocation == null
+                          ? "- m"
+                          : LocationUtils.formatDistance(
+                              attraction.location.toMarkerType(),
+                              currentLocation!,
+                            ),
+                      trailing: null,
+                    ),
+                    if (attraction.artist != null)
+                      PropertyInfo(
+                        leading: SvgPicture.asset(
+                          Assets.designerIconAsset,
+                          width: 24.0,
+                          height: 24.0,
+                          colorFilter: const ColorFilter.mode(
+                              Colors.white, BlendMode.srcIn),
+                        ),
+                        title: localizations.designer,
+                        text: attraction.artist!,
+                        trailing: null,
+                      ),
                   ],
                 ),
-                if (attraction.image != null)
-                  HeightConstrainedImage.network(
-                    height: 200,
-                    radius: 00,
-                    url: attraction.image!.image.url,
-                  ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          attraction.subTitle,
-                          textAlign: TextAlign.left,
-                          style: theme.textTheme.bodyMedium,
-                        ),
-                        if (attraction.description != null)
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0),
-                            child: Text(
-                              attraction.description!,
-                              textAlign: TextAlign.left,
-                              style: theme.textTheme.bodySmall,
-                            ),
-                          ),
-                      ],
-                    ),
-                  ),
-                ),
-                PropertyInfo(
-                  leading: SvgPicture.asset(
-                    Assets.attractionsIconAsset,
-                    width: 24.0,
-                    height: 24.0,
-                    colorFilter:
-                        const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                  ),
-                  title: localizations.category,
-                  text: getAttractionCategoryLabel(
-                      attraction.category, localizations),
-                  trailing: null,
-                ),
-                if (attraction.address != null)
-                  PropertyInfo(
-                    leading: SvgPicture.asset(
-                      Assets.homeIconAsset,
-                      width: 24.0,
-                      height: 24.0,
-                      colorFilter:
-                          const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                    ),
-                    title: localizations.address,
-                    text: attraction.address!,
-                    trailing: null,
-                  ),
-                PropertyInfo(
-                  leading: SvgPicture.asset(
-                    Assets.locationIconAsset,
-                    width: 24.0,
-                    height: 24.0,
-                    colorFilter:
-                        const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                  ),
-                  title: localizations.distanceToTarget,
-                  text: currentLocation == null
-                      ? "- m"
-                      : LocationUtils.formatDistance(
-                          attraction.location.toMarkerType(),
-                          currentLocation!,
-                        ),
-                  trailing: null,
-                ),
-                if (attraction.artist != null)
-                  PropertyInfo(
-                    leading: SvgPicture.asset(
-                      Assets.designerIconAsset,
-                      width: 24.0,
-                      height: 24.0,
-                      colorFilter:
-                          const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                    ),
-                    title: localizations.designer,
-                    text: attraction.artist!,
-                    trailing: null,
-                  ),
-              ],
+              ),
             ),
           ),
         ),
--- lib/widgets/coupon_overlay.dart
+++ lib/widgets/coupon_overlay.dart
@@ -36,127 +36,169 @@ class CouponOverlay extends StatelessWidget {
         child: SizedBox.expand(
           child: Container(
             color: Colors.black38,
-            child: Column(
-              children: [
-                AppBar(
-                  backgroundColor: Colors.black,
-                  leading: IconButton(
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                backgroundColor: Colors.black,
+                leading: IconButton(
+                  icon: const Icon(
+                    Icons.close,
+                    color: Colors.white,
+                  ),
+                  onPressed: onClose,
+                ),
+                title: Center(
+                  child: Text(
+                    benefit.title,
+                    style: theme.textTheme.bodyMedium,
+                  ),
+                ),
+                actions: [
+                  IconButton(
                     icon: const Icon(
-                      Icons.close,
+                      Icons.open_in_full,
                       color: Colors.white,
                     ),
-                    onPressed: onClose,
+                    onPressed: () {},
                   ),
-                  title: Center(
-                    child: Text(
-                      benefit.title,
-                      style: theme.textTheme.bodyMedium,
-                    ),
-                  ),
-                  actions: [
-                    IconButton(
-                      icon: const Icon(
-                        Icons.open_in_full,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-                if (benefit.image != null)
-                  HeightConstrainedImage.network(
-                    height: 200,
-                    radius: 00,
-                    url: benefit.image!.image.url,
-                  ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          localizations.benefitCoupon,
-                          textAlign: TextAlign.left,
-                          style: theme.textTheme.bodyMedium!
-                              .copyWith(fontSize: 10.0),
+                ],
+              ),
+              body: SingleChildScrollView(
+                child: Container(
+                  color: Colors.black38,
+                  child: Column(
+                    children: [
+                      if (benefit.image != null)
+                        HeightConstrainedImage.network(
+                          height: 200,
+                          radius: 00,
+                          url: benefit.image!.image.url,
                         ),
-                        Text(
-                          benefit.title,
-                          textAlign: TextAlign.left,
-                          style: theme.textTheme.bodyMedium,
-                        ),
-                        Text(
-                          benefit.benefitText,
-                          textAlign: TextAlign.left,
-                          style: theme.textTheme.bodyMedium!
-                              .copyWith(color: CustomThemeValues.appOrange),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8.0),
-                          child: Text(
-                            benefit.description,
-                            textAlign: TextAlign.left,
-                            style: theme.textTheme.bodySmall,
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                localizations.benefitCoupon,
+                                textAlign: TextAlign.left,
+                                style: theme.textTheme.bodyMedium!
+                                    .copyWith(fontSize: 10.0),
+                              ),
+                              Text(
+                                benefit.title,
+                                textAlign: TextAlign.left,
+                                style: theme.textTheme.bodyMedium,
+                              ),
+                              Text(
+                                benefit.benefitText,
+                                textAlign: TextAlign.left,
+                                style: theme.textTheme.bodyMedium!.copyWith(
+                                    color: CustomThemeValues.appOrange),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 8.0),
+                                child: Text(
+                                  benefit.description,
+                                  textAlign: TextAlign.left,
+                                  style: theme.textTheme.bodySmall,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-                if (partner.address != null)
-                  PropertyInfo(
-                    leading: SvgPicture.asset(
-                      Assets.homeIconAsset,
-                      width: 24.0,
-                      height: 24.0,
-                      colorFilter:
-                          const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                    ),
-                    title: localizations.address,
-                    text: partner.address!,
-                    trailing: null,
-                  ),
-                PropertyInfo(
-                  leading: SvgPicture.asset(
-                    Assets.locationIconAsset,
-                    width: 24.0,
-                    height: 24.0,
-                    colorFilter:
-                        const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                  ),
-                  title: localizations.distanceToTarget,
-                  text: currentLocation == null
-                      ? "- m"
-                      : LocationUtils.formatDistance(
-                          benefit.partner!.data!.partner.location
-                              .toMarkerType(),
-                          currentLocation!,
-                        ),
-                  trailing: null,
-                ),
-                PropertyInfo(
-                  leading: getPartnerCategoryIcon(partner.category),
-                  title:
-                      getPartnerCategoryLabel(partner.category, localizations),
-                  text: partner.name,
-                  trailing: null,
-                ),
-                if (partner.link != null)
-                  GestureDetector(
-                    onTap: () => launchUrl(Uri.parse(partner.link!)),
-                    child: PropertyInfo(
-                      leading: const Icon(
-                        Icons.open_in_new,
-                        color: Colors.white,
                       ),
-                      title: localizations.readMoreAbout,
-                      text: localizations.linkOpensInNewWindow,
-                      trailing: null,
-                    ),
+                      if (partner.address != null)
+                        PropertyInfo(
+                          leading: SvgPicture.asset(
+                            Assets.homeIconAsset,
+                            width: 24.0,
+                            height: 24.0,
+                            colorFilter: const ColorFilter.mode(
+                                Colors.white, BlendMode.srcIn),
+                          ),
+                          title: localizations.address,
+                          text: partner.address!,
+                          trailing: null,
+                        ),
+                      PropertyInfo(
+                        leading: SvgPicture.asset(
+                          Assets.locationIconAsset,
+                          width: 24.0,
+                          height: 24.0,
+                          colorFilter: const ColorFilter.mode(
+                              Colors.white, BlendMode.srcIn),
+                        ),
+                        title: localizations.distanceToTarget,
+                        text: currentLocation == null
+                            ? "- m"
+                            : LocationUtils.formatDistance(
+                                benefit.partner!.data!.partner.location
+                                    .toMarkerType(),
+                                currentLocation!,
+                              ),
+                        trailing: null,
+                      ),
+                      PropertyInfo(
+                        leading: getPartnerCategoryIcon(partner.category),
+                        title: getPartnerCategoryLabel(
+                            partner.category, localizations),
+                        text: partner.name,
+                        trailing: null,
+                      ),
+                      if (partner.link != null)
+                        GestureDetector(
+                          onTap: () => launchUrl(
+                            Uri.parse(partner.link!),
+                            mode: LaunchMode.externalApplication,
+                          ),
+                          child: PropertyInfo(
+                            leading: const Icon(
+                              Icons.open_in_new,
+                              color: Colors.white,
+                            ),
+                            title: localizations.readMoreAbout,
+                            text: localizations.linkOpensInNewWindow,
+                            trailing: null,
+                          ),
+                        ),
+                      IntrinsicWidth(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Expanded(
+                              child: OutlinedButton(
+                                style: theme.outlinedButtonTheme.style,
+                                onPressed: () {},
+                                child: Text(
+                                  localizations.close,
+                                  style: theme.textTheme.bodySmall!,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 10.0),
+                                child: OutlinedButton(
+                                  style: theme.outlinedButtonTheme.style,
+                                  onPressed: () {},
+                                  child: Text(
+                                    localizations.claimBenefit,
+                                    style: theme.textTheme.bodySmall!,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-              ],
+                ),
+              ),
             ),
           ),
         ),
