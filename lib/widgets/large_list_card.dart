8eb712e729defda209189ae8e150b7a61dc3683c
Lauri Lukkarinen <lauri.lukkarinen@metatavu.fi>
2023-09-18 16:28:59 +0300

    Fixed overflowing texts in benefits

--- lib/widgets/large_list_card.dart
+++ lib/widgets/large_list_card.dart
@@ -6,7 +6,6 @@ import "package:flutter_gen/gen_l10n/app_localizations.dart";
 import "package:valon_kaupunki_app/custom_theme_values.dart";
 import "package:valon_kaupunki_app/location_utils.dart";
 import "package:valon_kaupunki_app/widgets/height_constrained_image.dart";
-import "package:valon_kaupunki_app/widgets/property_info.dart";
 
 class LargeListCard extends StatelessWidget {
   final String? _imageUrl;
@@ -43,10 +42,10 @@ class LargeListCard extends StatelessWidget {
     final localizations = AppLocalizations.of(context)!;
 
     return Padding(
-      padding: const EdgeInsets.all(5.0),
+      padding: const EdgeInsets.all(5),
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5.0),
+          borderRadius: BorderRadius.circular(5),
           border: Border.all(color: CustomThemeValues.appOrange, width: 0.5),
           color: Colors.black38,
         ),
@@ -62,21 +61,21 @@ class LargeListCard extends StatelessWidget {
                   ),
                   child: HeightConstrainedImage.network(
                     height: 200,
-                    radius: 5.0,
+                    radius: 5,
                     url: _imageUrl!,
                   ),
                 ),
               Align(
                 alignment: Alignment.topLeft,
                 child: Padding(
-                  padding: const EdgeInsets.only(top: 8.0, left: 8.0),
+                  padding: const EdgeInsets.only(top: 8, left: 8),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         localizations.benefitCoupon,
                         style:
-                            theme.textTheme.bodySmall!.copyWith(fontSize: 12.0),
+                            theme.textTheme.bodySmall!.copyWith(fontSize: 12),
                       ),
                       Text(
                         _couponText,
@@ -90,33 +89,23 @@ class LargeListCard extends StatelessWidget {
                       if (_couponValidTo != null)
                         Text(
                           localizations.validUntil(_couponValidTo!),
-                          style: theme.textTheme.bodySmall!
-                              .copyWith(fontSize: 12.0),
-                        ),
-                      PropertyInfo(
-                        leading: getPartnerCategoryIcon(_partner.category),
-                        title: getPartnerCategoryLabel(
-                            _partner.category, localizations),
-                        text: _partner.name,
-                        trailing: Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: Row(
-                            children: [
-                              const Icon(
-                                Icons.location_on_outlined,
-                                color: Colors.white,
-                                opticalSize: 24.0,
-                              ),
-                              Text(
-                                _currentLocation == null
-                                    ? "- m"
-                                    : LocationUtils.formatDistance(
-                                        _partner.location.toMarkerType(),
-                                        _currentLocation!),
-                                style: theme.textTheme.bodySmall,
-                              ),
-                            ],
+                          style: theme.textTheme.bodySmall!.copyWith(
+                            fontSize: 12,
                           ),
+                        ),
+                      ListTile(
+                        leading: getPartnerCategoryIcon(_partner.category),
+                        title: Text(
+                          getPartnerCategoryLabel(
+                            _partner.category,
+                            localizations,
+                          ),
+                          style: theme.textTheme.bodyMedium!
+                              .copyWith(fontSize: 14),
+                        ),
+                        subtitle: Text(
+                          _partner.name,
+                          style: theme.textTheme.bodySmall,
                         ),
                       ),
                     ],
@@ -124,29 +113,53 @@ class LargeListCard extends StatelessWidget {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: OutlinedButton(
-                    onPressed: _readMore,
-                    style: _readMore == null
-                        ? theme.outlinedButtonTheme.style!.copyWith(
-                            backgroundColor:
-                                const MaterialStatePropertyAll(Colors.grey),
-                            side: const MaterialStatePropertyAll(
-                                BorderSide(style: BorderStyle.none)),
-                          )
-                        : theme.outlinedButtonTheme.style,
-                    child: Text(
-                      _readMore == null
-                          ? localizations.claimed
-                          : localizations.readMore,
-                      style: _readMore == null
-                          ? const TextStyle(color: Colors.white)
-                          : theme.outlinedButtonTheme.style!.textStyle!
-                              .resolve({}),
+                padding: const EdgeInsets.only(right: 8, bottom: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 27, right: 12),
+                          child: Icon(
+                            Icons.location_on_outlined,
+                            color: Colors.white,
+                            opticalSize: 24,
+                          ),
+                        ),
+                        Text(
+                          _currentLocation == null
+                              ? "- m"
+                              : LocationUtils.formatDistance(
+                                  _partner.location.toMarkerType(),
+                                  _currentLocation!,
+                                ),
+                          style: theme.textTheme.bodySmall,
+                        ),
+                      ],
                     ),
-                  ),
+                    OutlinedButton(
+                      onPressed: _readMore,
+                      style: _readMore == null
+                          ? theme.outlinedButtonTheme.style!.copyWith(
+                              backgroundColor:
+                                  const MaterialStatePropertyAll(Colors.grey),
+                              side: const MaterialStatePropertyAll(
+                                BorderSide(style: BorderStyle.none),
+                              ),
+                            )
+                          : theme.outlinedButtonTheme.style,
+                      child: Text(
+                        _readMore == null
+                            ? localizations.claimed
+                            : localizations.readMore,
+                        style: _readMore == null
+                            ? const TextStyle(color: Colors.white)
+                            : theme.outlinedButtonTheme.style!.textStyle!
+                                .resolve({}),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
